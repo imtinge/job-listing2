e4ab2fbcc18a1d22c9acc891ac048d1a6c164386
田汀 <imtinge@gmail.com>
2017-04-09 21:48:34 +0800

    实作 Job 的 CRUD （ JobsController ), 先只开 Job 的 title, description, （最小可行性界面）

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,52 @@
+class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+    @job = Job.new(job_params)
+    
+    if @job.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def udpate
+    if @job.update(job_params)
+      redirect_to root_path
+    else
+      render :edit, notice: "Update success"
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to root_path, notice: "Job deleted"
+  end
+
+  private
+    
+    def set_job
+      @job = Job.find(params[:id])
+    end
+    
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
+end
