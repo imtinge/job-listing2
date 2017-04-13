fa6df2650b257e513550c31127a6bd3895ff0cb1
田汀 <tanagisa@tiantingdeMacBook-Air.local>
2017-04-13 23:31:37 +0800

    改了一个比较看得过去的landing page，闪工网

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,11 +1,13 @@
 module FlashesHelper
-  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
+  FLASH_CLASSES = {
+    info: "info", alert: "danger", notice: "success", warning: "warning"
+  }.freeze
 
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
-    flash.to_hash.slice "alert", "notice", "warning" 
+    flash.to_hash.slice "alert", "notice", "warning", "info"
   end
 end
